3426e8f10a22fb423904805c77455ac7cdf2ef1d
Gyusun Yeom <omniavinco@gmail.com>
2017-04-03 08:52:25 +0900

    Render into pdf with pdfkit(wkhtmltopdf) #2

--- grip_one/cli.py
+++ grip_one/cli.py
@@ -3,15 +3,17 @@ from getpass import getpass
 from sys import stdout
 
 from .lib import Renderer
+from pdfkit import from_string
 
 def create_argparser():
 	parser = ArgumentParser(description="Make single html page from Markdown repo")
 	parser.add_argument("repo_root", help="Repository root to render")
 	parser.add_argument("--entry", default="README.md", help="custom entry md file")
 	parser.add_argument("--offline", action="store_true", help="use offline mode")
-	parser.add_argument("--out", default="-", help="where to out")
+	parser.add_argument("--out", default="-", help="where to out - available etension is html or pdf")
 	parser.add_argument("--login", action="store_true", help="login github to extend API limit")
 	parser.add_argument("--embed", action="store_true", help="embed images into html in base64 form")
+	parser.add_argument("--pdf", choices=["disable", "pdfkit"], default="disable", help="Select backend to use for making pdf")
 	return parser
 
 def main(parser=None):
@@ -23,7 +25,8 @@ def main(parser=None):
 		"grip": {
 			"render_offline": args.offline
 		},
-		"embed_img": args.embed
+		"embed_img": args.embed,
+		"pdf": args.pdf,
 	}
 
 	# prepare login option
@@ -36,12 +39,21 @@ def main(parser=None):
 	renderer = Renderer(args.repo_root, args.entry, render_option)
 	full_article = renderer.render_all()
 
+	full_article_str = full_article.prettify()
+
+	if args.pdf == "disable":
+		out_content = full_article_str.encode("utf-8")
+	elif args.pdf == "pdfkit":
+		out_content = from_string(full_article_str, False)
+	else:
+		raise Exception("invalid pdf mode")
+
 	if args.out == "-":
 		out = stdout
 	else:
-		out = open(args.out, "w")
+		out = open(args.out, "wb")
 
-	out.write(full_article.prettify())
+	out.write(out_content)
 
 	if out is not stdout:
 		out.close()
